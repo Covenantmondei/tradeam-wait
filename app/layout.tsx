712f54d52a83e7c3833321583a9bb4a23f83d9a7
Covenant Monday <covenantmonday863@gmail.com>
2025-07-04 14:27:31 +0100

    changed name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'TradeAm',
+  description: 'No Middleman, No Wahala',
+  generator: 'Covenant Monday',
 }
 
 export default function RootLayout({
